9e890196a429d9a79c856da41e75863654de7bde
paveldvoryak <p380970610647@gmail.com>
2017-12-12 19:18:49 +0200

    printTree method added

--- src/com/company/task1/Generator.java
+++ src/com/company/task1/Generator.java
@@ -6,19 +6,17 @@ public class Generator {
 
     public static List<Integer> generateList(int count, int from, int to) {
         ArrayList<Integer> out = new ArrayList<>(count);
-        generate(out,count,from,to);
+        for(int i = 0; i < count; i++) {
+            out.add((new Random().nextInt(to - from) + from));
+        }
         return out;
     }
 
     public static Set<Integer> generateSet(int count, int from, int to) {
         Set<Integer> out = new HashSet<>(count);
-        generate(out,count,from,to);
-        return out;
-    }
-
-    private static void generate(Collection<Integer> collection, int count, int from, int to) {
-        for(int i = 0; i < count; i++) {
-            collection.add((new Random().nextInt(to - from) + from));
+        while(out.size() != count) {
+            out.add((new Random().nextInt(to - from) + from));
         }
+        return out;
     }
 }
