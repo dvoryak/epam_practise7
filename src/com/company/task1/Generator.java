597254884780bf35a79339e25fd4a38e2b890391
paveldvoryak <p380970610647@gmail.com>
2017-12-12 19:24:39 +0200

    printTree method added

--- src/com/company/task1/Generator.java
+++ src/com/company/task1/Generator.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class Generator {
 
     public static List<Integer> generateList(int count, int from, int to) {
+        checkOf(count, from, to);
         ArrayList<Integer> out = new ArrayList<>(count);
         for(int i = 0; i < count; i++) {
             out.add((new Random().nextInt(to - from) + from));
@@ -13,10 +14,17 @@ public class Generator {
     }
 
     public static Set<Integer> generateSet(int count, int from, int to) {
+        checkOf(count, from, to);
         Set<Integer> out = new HashSet<>(count);
         while(out.size() != count) {
             out.add((new Random().nextInt(to - from) + from));
         }
         return out;
     }
+
+    private static void checkOf(int count, int from, int to) {
+        if(count < 0 || to < from) throw new IllegalArgumentException();
+    }
+
+
 }
--- src/com/company/task1/Main.java
+++ src/com/company/task1/Main.java
@@ -3,7 +3,7 @@ package com.company.task1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("List: " + Generator.generateList(10,0,10));
-        System.out.println("Set: " + Generator.generateSet(10,0,10));
+        System.out.println("List: " + Generator.generateList(5,0,10));
+        System.out.println("Set: " + Generator.generateSet(5,0,10));
     }
 }
